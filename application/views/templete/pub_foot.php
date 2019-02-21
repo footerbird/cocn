9a23a6ad3625f872bab6aa1e54666056fd97e360
footerbird <footerbird@163.com>
2019-02-21 10:27:35 +0800

    fixed 引用文件路径错误

--- application/views/templete/pub_foot.php
+++ application/views/templete/pub_foot.php
@@ -1,6 +1,6 @@
-<script src="/htdocs/waitui/js/jquery-1.11.1.min.js?<?php echo CACHE_TIME; ?>"></script>
-<script src="/htdocs/waitui/js/public.js?<?php echo CACHE_TIME; ?>"></script>
-<script src="/htdocs/waitui/js/dom-ready.js?<?php echo CACHE_TIME; ?>"></script>
+<script src="/htdocs/js/jquery-1.11.1.min.js?<?php echo CACHE_TIME; ?>"></script>
+<script src="/htdocs/js/public.js?<?php echo CACHE_TIME; ?>"></script>
+<script src="/htdocs/js/dom-ready.js?<?php echo CACHE_TIME; ?>"></script>
 <script type="text/javascript">
 $(function(){
     
--- application/views/templete/pub_head.php
+++ application/views/templete/pub_head.php
@@ -5,5 +5,5 @@
 <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no' />
 <link rel="icon" href="/htdocs/images/favicon.ico" type="image/x-icon">
 <link rel="shortcut icon" href="/htdocs/images/favicon.ico" type="image/x-icon">
-<link rel="stylesheet" href="/htdocs/waitui/css/public.css?<?php echo CACHE_TIME; ?>">
+<link rel="stylesheet" href="/htdocs/css/public.css?<?php echo CACHE_TIME; ?>">
 
