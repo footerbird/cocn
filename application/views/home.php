3e07742a443b0aa712911e902d964c5442f4d9d4
xunijy <412294794@qq.com>
2019-02-21 10:31:51 +0800

    xiugai
    xx

--- application/views/home.php
+++ application/views/home.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <title>标题</title>
+  <meta name="Keywords" content="关键词"/>
+  <meta name="description" content="描述"/>
 <?php include_once('templete/pub_head.php') ?>
 </head>
 <body>
 hello home
-
+<div class="bg-gray3">
+  ss
+</div>
 <?php include_once('templete/pub_foot.php') ?>
 <script type="text/javascript">
 $(function(){
-    
+
 })
 </script>
 </body>
--- application/views/templete/pub_foot.php
+++ application/views/templete/pub_foot.php
@@ -1,8 +1,8 @@
-<script src="/htdocs/waitui/js/jquery-1.11.1.min.js?<?php echo CACHE_TIME; ?>"></script>
-<script src="/htdocs/waitui/js/public.js?<?php echo CACHE_TIME; ?>"></script>
-<script src="/htdocs/waitui/js/dom-ready.js?<?php echo CACHE_TIME; ?>"></script>
+<script src="/htdocs/js/jquery-1.11.1.min.js?<?php echo CACHE_TIME; ?>"></script>
+<script src="/htdocs/js/public.js?<?php echo CACHE_TIME; ?>"></script>
+<script src="/htdocs/js/dom-ready.js?<?php echo CACHE_TIME; ?>"></script>
 <script type="text/javascript">
 $(function(){
-    
+
 })
 </script>
