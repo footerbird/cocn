a5dd9d71bec62c090f75dbc5e63c093b90a8dd84
xunijy <412294794@qq.com>
2019-02-28 15:17:18 +0800

    信用查询结果页
    补充导航用户下拉

--- application/views/templete/pub_nav.php
+++ application/views/templete/pub_nav.php
@@ -4,13 +4,13 @@
         <a href="/page/home.php" class="main-logo fl-l mb10">co.cn</a>
         <div class="nav-login pos-rela">
             <a href="javascript:;" class="dl">登录</a><em class="col-default">/</em><a href="" class="zc">注册</a>
-            <a href="javascript:;" class="user pos-rela" id="">
+            <a href="javascript:;" class="user pos-rela" id="user">
               <span class="">
                 <img src="" alt="" class="head-img">
                 <i class="ico-arrow fl-l ml6 mt20"></i>
               </span>
             </a>
-            <div class="nav-user-pannel pos-abso">
+            <div class="nav-user-pannel pos-abso" id="nav_user_pannel" style="display:none">
               <ul>
                 <li class="name">
                   <a href="javascript:;">XXXXXX</a>
@@ -61,7 +61,6 @@
             </li>
           </ul>
         </div>
-
     </div>
   </div>
 </div>
