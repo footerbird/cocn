a5ec9fd4653e0f5f93acf4d515ed98ebdb13c89c
xunijy <412294794@qq.com>
2019-02-28 16:07:55 +0800

    企业信用查询

--- application/views/templete/pub_foot.php
+++ application/views/templete/pub_foot.php
@@ -3,6 +3,14 @@
 <script src="/htdocs/js/dom-ready.js?<?php echo CACHE_TIME; ?>"></script>
 <script type="text/javascript">
 $(function(){
-
+  $("#user").on("click",function(){//点击用户菜单下拉
+      if($(this).hasClass("on")) {
+          $(this).removeClass("on");
+          $("#nav_user_pannel").hide();
+      }else {
+          $(this).addClass("on");
+          $("#nav_user_pannel").show();
+      }
+  })
 })
 </script>
