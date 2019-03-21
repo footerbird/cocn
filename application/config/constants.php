f943f38bdd414d2336141923a2276a5f71a94ff9
xunijy <412294794@qq.com>
2019-03-21 17:07:08 +0800

    补充详细页改色

--- application/config/constants.php
+++ application/config/constants.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //更新CSS JS缓存时间
-define('CACHE_TIME', '2019032101701');
+define('CACHE_TIME', '2019032101706');
 
 defined('SHOW_DEBUG_BACKTRACE') OR define('SHOW_DEBUG_BACKTRACE', TRUE);
 
