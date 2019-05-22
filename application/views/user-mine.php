e3ca282eeaa2b6adfe3cbbfcfd96edd7e538e964
xunijy <412294794@qq.com>
2019-05-22 11:37:28 +0800

    个人中心

---
+++ application/views/user-mine.php
@@ -0,0 +1,314 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once('templete/pub_head_london.php') ?>
+</head>
+<body>
+<?php include_once('templete/pub_nav_inside.php') ?>
+<!-- 用户中心开始 -->
+<div class="container-full">
+  <?php include_once('templete/pub_leftnav.php') ?>
+  <div class="view-frt">
+    <div class="all-full">
+      <div class="all-flt">
+        <div class="mine-box after-cls mb15">
+          <div class="cocn-kaitong">
+            <p class="ta-c f18 col-default pt15 pb30">
+              您尚未开立股权投资账户
+            </p>
+            <img src="/htdocs/images/inner-no-kaitong.png" alt="" class="m-auto">
+            <p class="ta-c col-gray9 lh28 pt30">
+              您好，进行基本信息录入、证件审核、银行收款信息设置等三步操作，<br />即可开立您的投资账户，开始股权投资和交易。
+            </p>
+            <div class="ta-c pb15 pt30">
+              <a href="/page/certify-person" class="w190 pub-btn">
+                立即开通
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="mine-box mb15">
+          <div class="after-cls w-all">
+            <div class="fl-l f16 col-default">s先生，下午好</div>
+            <div class="fl-r">
+              <div class="block-a-renzheng mr8">身份认证</div>
+              <div class="block-a-zanwu mr8">身份认证</div>
+              <div class="block-a-zhishu mr8">资金账户</div>
+              <div class="block-a-zanwu mr8">资金账户</div>
+              <div class="block-a-disanfang mr8">第三方认证</div>
+              <div class="block-a-zanwu">第三方认证</div>
+            </div>
+            <p class="f12 col-gray9 fl-l w-all pb5 lh30"><em class="in-block mr40">注册日期&nbsp;&nbsp;&nbsp;&nbsp;2018-12-24</em><em class="in-block mr40">最近登录&nbsp;&nbsp;&nbsp;&nbsp;2019-02-28 13:35</em></p>
+          </div>
+          <div class="pl15 pr15 mt8 mb25"><div class="hr "></div></div>
+          <div class="f16 col-default ta-c mb20">您未持有公司股权，进入<a href="/page/trading-center" class="col-3daaef">股权交易中心</a>选择股权投资</div>
+          <table class="">
+            <tbody>
+              <tr class="col-default">
+                <td>持股公司<em class="col-f4364c ml12 in-block mr50">3</em></td>
+                <td>股权交易<em class="col-f4364c ml12 in-block mr50">33</em></td>
+                <td>待回复<em class="col-gray9 ml12 in-block mr50">0</em></td>
+                <td>历史成交次数<em class="col-gray9 ml12 in-block mr50">0</em></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="mine-box mb15">
+          <div class="bg-f6 padding20">
+            <div class="pos-rela col-default w-all ml5">杭州三六五网络有限公司</div>
+            <div class="w-all after-cls">
+              <table class="fl-r mr5 mt5 mb5">
+                <tbody>
+                  <tr class="">
+                    <td>持股<em class="col-f4364c ml12 in-block mr50">10%</em></td>
+                    <td>冻结<em class="col-f4364c ml12 in-block mr50">5.7%</em></td>
+                    <td>可流通<em class="col-f4364c ml12 in-block ">5.7%</em></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="w-all after-cls">
+              <table class="ml5 f12">
+                <tbody>
+                  <tr class="col-gray9">
+                    <td>注册资本<em class="col-gray3 ml12 in-block mr50">5000万元人民币</em></td>
+                    <td>成立日期<em class="col-gray3 ml12 in-block mr50">2018-12-24</em></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="w-all after-cls">
+            <table width="100%" class="f12 list mb15">
+              <tbody>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">发起股权出售 5.3%</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">完成交易，购买 10% 股权</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="f12 pl24 mb10">其他相关记录</div>
+          <div class="w-all after-cls">
+            <table width="100%" class="f12 list">
+              <tbody>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">发起股权出售 5.3%</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">完成 3.5% 股权交易</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">由法定代表人开通公司股权交易</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="mine-box mb15">
+          <div class="bg-f6 padding20">
+            <div class="pos-rela col-default w-all ml5">杭州名商网络有限公司
+              <em class="block-a-zhishu ml5 pos-abso t-4">法人</em>
+            </div>
+            <div class="w-all after-cls">
+              <table class="fl-r mr5 mt5 mb5">
+                <tbody>
+                  <tr class="">
+                    <td>持股<em class="col-f4364c ml12 in-block mr50">10%</em></td>
+                    <td>冻结<em class="col-f4364c ml12 in-block mr50">5.7%</em></td>
+                    <td>可流通<em class="col-f4364c ml12 in-block ">5.7%</em></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="w-all after-cls">
+              <table class="ml5 f12">
+                <tbody>
+                  <tr class="col-gray9">
+                    <td>注册资本<em class="col-gray3 ml12 in-block mr50">5000万元人民币</em></td>
+                    <td>成立日期<em class="col-gray3 ml12 in-block mr50">2018-12-24</em></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="w-all after-cls">
+            <table width="100%" class="f12 list mb15">
+              <tbody>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">发起股权出售 5.3%</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">完成交易，购买 10% 股权</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="f12 pl24 mb10">其他相关记录</div>
+          <div class="w-all after-cls">
+            <table width="100%" class="f12 list">
+              <tbody>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">发起股权出售 5.3%</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">用户评论：这家公司存在一些问题，公司注册地址是空的。这家公司存在一些问题，公司注册地址是空的。这家公司存在一些问题，公司注册地址是空的。这家公司存在一些问题，公司注册地址是空的。</td>
+                  <td width="20%"><a href="/page/user-cocn-comment" class="col-3daaef">我要辟谣&nbsp;&gt;</a>
+                  </td>
+                </tr>
+                <tr class="">
+                  <td width="20%">2019-02-21 18:00:05</td>
+                  <td width="60%">发起股权出售 10.3%</td>
+                  <td width="20%">&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="mine-box">
+          <div class="f20 ta-c col-default lh30 mb35">快速导航</div>
+          <div class="w-all">
+            <table width="80%" class="col-default m-auto lh38">
+              <tbody>
+                <tr class="ta-c">
+                  <td width="20%">
+                    <a href="/page/user-personal" class="in-block col-default"><i class="ico-mine-sf"></i>
+                    <p>身份认证</p></a>
+                  </td>
+                  <td width="20%">
+                    <a href="/page/user-accounts" class="in-block col-default"><i class="ico-mine-zj"></i>
+                    <p>资金账户</p></a>
+                  </td>
+                  <td width="20%">
+                    <a href="/page/user-company-certify" class="in-block col-default"><i class="ico-mine-gs"></i>
+                    <p>公司认证</p></a>
+                  </td>
+                  <td width="20%">
+                    <a href="/page/" class="in-block col-default"><i class="ico-mine-qy"></i>
+                    <p>关注区域</p></a>
+                  </td>
+                  <td width="20%">
+                    <a href="/page/" class="in-block col-default"><i class="ico-mine-hy"></i>
+                    <p>关注行业</p></a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="all-frt">
+        <div class="mine-box mb15 after-cls padding20">
+          <div class="ta-c col-default lh30 mb20 mt5 f20">官网认证</div>
+          <p class="ta-c lh20">经检测，您持有<em class="col-f4364c in-block ml4 mr4">3</em>家公司未开启官网认证</p>
+          <p class="mt40 mb25 ta-c">
+            <a href="/page/certify-web" class="pub-btn w180">点击认证</a>
+          </p>
+        </div>
+        <div class="mine-r-box mb15">
+          <div class="hb">重要信息提醒</div>
+          <div class="bb">
+            <table class="f12 list">
+              <tbody>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">名商网络：再资本投入</td>
+                </tr>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">名商网络：再追加1000万元资本投入</td>
+                </tr>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">官网：房地产类资产分析报价走势稳定</td>
+                </tr>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">孔方正：中美贸易</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="mine-r-box mb15">
+          <div class="hb">24小时直播</div>
+          <div class="bb">
+            <table class="f12 list">
+              <tbody>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">名商网络：再资本投入</td>
+                </tr>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">名商网络：再追加1000万元资本投入</td>
+                </tr>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">官网：房地产类资产分析报价走势稳定</td>
+                </tr>
+                <tr>
+                  <td width="15%" class="col-gray9">11:22</td>
+                  <td width="85%">孔方正：中美贸易</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="mine-r-box">
+          <div class="hb">我的关注</div>
+          <div class="bb w-all pt20 pb20">
+            <table width="90%" class="m-auto">
+              <tbody>
+                <tr class="lh28" align="center">
+                  <td>
+                    <p class="f20 col-f4364c"><a href="" class="col-f4364c">10</a></p>
+                    <p>家公司</p>
+                  </td>
+                  <td>
+                    <p class="f20 col-f4364c"><a href="" class="col-f4364c">1</a></p>
+                    <p>个地区</p>
+                  </td>
+                  <td>
+                    <p class="f20 col-f4364c"><a href="" class="col-f4364c">9</a></p>
+                    <p>个行业</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- footer -->
+<?php include_once('templete/pub_foot_inside.php') ?>
+<script src="/htdocs/js/slick/slick.min.js?<?php echo CACHE_TIME; ?>"></script>
+<link rel="stylesheet" href="/htdocs/js/slick/slick.css?<?php echo CACHE_TIME; ?>">
+<script type="text/javascript">
+$(function(){
+})
+</script>
+</body>
+</html>
