e4b636ea2668287cd08aaa42a29298b1b0e91873
xunijy <412294794@qq.com>
2019-06-27 17:42:59 +0800

    公司信用查询首页

---
+++ application/views/company-index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once('templete/pub_head_london.php') ?>
+</head>
+<body class="big">
+<?php include_once('templete/pub_nav.php') ?>
+<!-- 公司认证-->
+<div class="container">
+  <div class="company-big-box after-cls">
+    <div class="title">公司信用查询</div>
+    <div class="search-box after-cls">
+      <input type="text" name="" value="" placeholder="查询公司名称、法人或经营范围">
+      <a href="/page/company-credit-list.php" class="btn"></a>
+    </div>
+    <div class="hot">
+      <em class="mr10 fl-l in-block">热搜榜：</em>
+      <a href=/page/company-credit-page-simple.php"">杭州名商网络科技有限公司</a><a href=/page/company-credit-page-simple.php"">杭州名商网络科技有限公司</a><a href=/page/company-credit-page-simple.php"">名商网络科技有限公司</a>
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
