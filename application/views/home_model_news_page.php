117b1502bf27c9c1622625c8e8bc657cc28c9ce0
xunijy <412294794@qq.com>
2019-07-05 10:15:27 +0800

    新闻详情

---
+++ application/views/home_model_news_page.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once('templete/pub_head_model.php') ?>
+</head>
+<body>
+<!-- 开始 -->
+<div class="pub-head" id="pub_head">
+  <div class="container">
+    <a href="/page/home_model" class="main-logo fl-l"></a>
+    <div class="fl-r">
+      <a href="javascript:;" id="headerToggle" class="header__toggle"><span></span></a>
+      <ul class="main-nav clearfix">
+        <li>
+          <a href="/page/home_model">首页</a>
+        </li>
+        <li>
+          <a href="/page/home_model_list">产品列表</a>
+        </li>
+        <li>
+          <a href="/page/home_model_about">关于我们</a>
+        </li>
+        <li>
+          <a href="/page/home_model_news" class="on">新闻中心</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+<div class="bgblack" id="bgblack"></div>
+<div class="container after-cls bg-white">
+  <div class="news-page after-cls">
+    <div class="news-hd after-cls">
+      <div class="fl-l w-all after-cls">
+        <div class="title">杭州官网科技有限公司开通股权交易服务</div>
+      </div>
+      <!-- <div class="hd after-cls fl-l w-all">
+        <a href="javascript:;" class="col-gray9 mr10"><i class="ico-zhuanfa mr5"></i>3344</a>
+        <a href="javascript:;" class="col-gray9 mr10"><i class="ico-pinlun mr5"></i>2</a>
+        <a href="javascript:;" class="col-gray9 mr10"><i class="ico-zan mr5"></i>33</a>
+        <a href="javascript:;" class="col-gray9 mr10"><i class="ico-shoucang mr5"></i>99</a>
+        <a href="javascript:;" class="col-3daaef">投诉</a>
+      </div> -->
+    </div>
+    <div class="news-content">
+      <div class="news-desc">伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。
+      </div>
+      <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+      <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+      <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+      <p>
+        <img src="http://iph.href.lu/830x320?text=830*320" width="100%" height="" alt="">
+        <span></span>
+      </p>
+      <div class="hr mb20 mt20"></div>
+    </div>
+    <div class="news-comm">
+      <div class="news-comm-con">
+        <dl class="">
+          <dt>
+            <textarea name="name" rows="" cols="" placeholder="说点什么"></textarea>
+          </dt>
+          <dd>
+            <a href="javascript:;" class="pub-btn">发布评论</a>
+          </dd>
+        </dl>
+      </div>
+      <div class="news-comm-list">
+        <div class="hd">评论</div>
+        <div class="hr mb15 mt15"></div>
+        <dl class="">
+          <dt class="after-cls">
+            <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+            <p class="mt5"></p>
+          </dt>
+          <dd>
+            <em class="col-gray3 mr5">利群</em>
+            <em class="col-gray9 mr5">2019-05-23 11:09</em>
+            <a href="javascript:;" class="fl-r col-3daaef">不实信息举报</a>
+          </dd>
+        </dl>
+        <dl class="">
+          <dt class="after-cls">
+            <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+            <p class="mt5"></p>
+          </dt>
+          <dd>
+            <em class="col-gray3 mr5">利群</em>
+            <em class="col-gray9 mr5">2019-05-23 11:09</em>
+            <a href="javascript:;" class="fl-r col-3daaef">不实信息举报</a>
+          </dd>
+        </dl>
+        <dl class="">
+          <dt class="after-cls">
+            <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+            <p class="mt5"></p>
+          </dt>
+          <dd>
+            <em class="col-gray3 mr5">利群</em>
+            <em class="col-gray9 mr5">2019-05-23 11:09</em>
+            <a href="javascript:;" class="fl-r col-3daaef">不实信息举报</a>
+          </dd>
+        </dl>
+        <dl class="">
+          <dt class="after-cls">
+            <p>伴随公司股权交易系统的开发，已经完成第一轮资金投资，官网科技成立3年后正式开通股权交易服务，本次股权出售比例为 3.9%，募资预计达到 300 万元人民币，本次募资主要投入系统开发和基础上运营。</p>
+            <p class="mt5"></p>
+          </dt>
+          <dd>
+            <em class="col-gray3 mr5">利群</em>
+            <em class="col-gray9 mr5">2019-05-23 11:09</em>
+            <a href="javascript:;" class="fl-r col-3daaef">不实信息举报</a>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include_once('templete/pub_foot_model.php') ?>
+<script type="text/javascript">
+$(function(){
+  $("#headerToggle").on('click',function () {
+    $("#pub_head").toggleClass('pub-head-on');
+    $("body").toggleClass('on');
+  });
+  $("#bgblack").on('click',function () {
+    $("#pub_head").removeClass('pub-head-on');
+    $("body").removeClass('on');
+
+  })
+})
+</script>
+</body>
+</html>
