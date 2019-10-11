39b8b37a31197a5cc59d3b410d69654c57cf0efc
xunijy <412294794@qq.com>
2019-10-11 17:57:19 +0800

    公司信用查询v2

---
+++ application/views/company-credit-list2.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once('templete/pub_head_london.php') ?>
+</head>
+<body>
+<?php include_once('templete/pub_nav_a.php') ?>
+<!-- 查询 -->
+<div class="container mb20 mt20">
+  <div class="list-center after-cls">
+    <div class="cocn-list2">
+      <div class="hd after-cls">
+        <span class="col-default f14 lh30" id="">共找到<em   class="col-f4364c in-block f-thin ml4 mr4">229</em>个公司</span>
+        <div class="right">
+          <a href="#" class="arrow-l mr5"></a>
+          <a href="#" class="arrow-r"></a>
+        </div>
+      </div>
+        <div class="bd">
+          <div class="box certified">
+            <div class="top">
+              <div class="title">
+                <a class="f18" href="/page/company-credit-page-simple.php">我是<em class="col-f4364c">简版</em>详情页 点我</a>
+                <a href="javascript:;" class="abtn-a-renzheng">认证</a>
+                <a href="javascript:;" class="abtn-a-zaiye">在业</a>
+                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
+                <a href="#" class="abtn-a-guanzhu">关注</a>
+              </div>
+              <div class="info">
+                <span>法定代表人： 谢青弟</span>
+                <span>注册资本：1000万元人民币</span>
+                <span>成立时间：2003-08-01</span>
+                <span>所属行业：批发和零售业</span>
+                <span>地址：南京市鼓楼区建宁路37号</span>
+              </div>
+            </div>
+            <div class="center">
+              <ul class="list">
+                <li>
+                  <a href="#">名商网代理发布华为 P30 pro 购机享分期免息<em>2019-12-09</em></a>
+                </li>
+                <li>
+                  <a href="#">新开发者限时优惠。低至9元起！<em>2019-12-09</em></a>
+                </li>
+                <li>
+                  <a href="#">名商网代理。华为交换机/服务器/存储等在线购买<em>2019-12-09</em></a>
+                </li>
+              </ul>
+              <table width="100%">
+                <thead>
+                  <tr>
+                    <th width="154">公司代码</th>
+                    <th width="154">出售股权</th>
+                    <th width="205">总交易额<em>（元/人民币）</em></th>
+                    <th width="205">最小交易额<em>（元/人民币）</em></th>
+                    <th width="205">历史成交额<em>（万元/人民币）</em></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>907701</td>
+                    <td>12%</td>
+                    <td>92000000</td>
+                    <td>50000</td>
+                    <td>12.5</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="ft after-cls">
+              <a href="#" class="buy">立即购买</a>
+              <a href="#" class="buy2">发起股权认购</a>
+            </div>
+          </div>
+          <div class="box certified">
+            <div class="top">
+              <div class="title">
+                <a class="f18" href="/page/company-credit-page.php">我是<em class="col-f4364c">正常版</em>详情页 点我</a>
+                <a href="javascript:;" class="abtn-a-renzheng">认证</a>
+                <a href="javascript:;" class="abtn-a-zaiye-had">在业</a>
+                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
+                <a href="#" class="abtn-a-guanzhu had">已关注</a>
+              </div>
+              <div class="info">
+                <span>法定代表人： 谢青弟</span>
+                <span>注册资本：1000万元人民币</span>
+                <span>成立时间：2003-08-01</span>
+                <span>所属行业：批发和零售业</span>
+                <span>地址：南京市鼓楼区建宁路37号</span>
+              </div>
+            </div>
+            <div class="center">
+              <ul class="list">
+                <li>
+                  <a href="#">名商网代理发布华为 P30 pro 购机享分期免息<em>2019-12-09</em></a>
+                </li>
+                <li>
+                  <a href="#">新开发者限时优惠。低至9元起！<em>2019-12-09</em></a>
+                </li>
+                <li>
+                  <a href="#">名商网代理。华为交换机/服务器/存储等在线购买<em>2019-12-09</em></a>
+                </li>
+              </ul>
+              <table width="100%">
+                <thead>
+                  <tr>
+                    <th width="154">公司代码</th>
+                    <th width="154">出售股权</th>
+                    <th width="205">总交易额<em>（元/人民币）</em></th>
+                    <th width="205">最小交易额<em>（元/人民币）</em></th>
+                    <th width="205">历史成交额<em>（万元/人民币）</em></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>907701</td>
+                    <td>12%</td>
+                    <td>92000000</td>
+                    <td>50000</td>
+                    <td>12.5</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="ft after-cls">
+              <a href="#" class="buy3">暂无在售股权</a>
+              <a href="#" class="buy2">发起股权认购</a>
+            </div>
+          </div>
+          <div class="box">
+            <div class="top">
+              <div class="title">
+                <a class="f18" href="/page/company-credit-page.php">我是<em class="col-f4364c">正常版</em>详情页 点我</a>
+                <a href="javascript:;" class="abtn-a-zaiye">在业</a>
+                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
+                <a href="#" class="abtn-a-guanzhu had">已关注</a>
+                <a href="#" class="abtn-a-kaitong">我是法人，申请开通官网</a>
+              </div>
+              <div class="info">
+                <span>法定代表人： 谢青弟</span>
+                <span>注册资本：1000万元人民币</span>
+                <span>成立时间：2003-08-01</span>
+                <span>所属行业：批发和零售业</span>
+                <span>地址：南京市鼓楼区建宁路37号</span>
+              </div>
+            </div>
+          </div>
+          <div class="box">
+            <div class="top">
+              <div class="title">
+                <a class="f18" href="/page/company-credit-page.php">我是<em class="col-f4364c">正常版</em>详情页 点我</a>
+                <a href="javascript:;" class="abtn-a-zaiye-had">注销</a>
+                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
+                <a href="#" class="abtn-a-guanzhu">关注</a>
+                <a href="#" class="abtn-a-kaitong">我是法人，申请开通官网</a>
+              </div>
+              <div class="info">
+                <span>法定代表人： 谢青弟</span>
+                <span>注册资本：1000万元人民币</span>
+                <span>成立时间：2003-08-01</span>
+                <span>所属行业：批发和零售业</span>
+                <span>地址：南京市鼓楼区建宁路37号</span>
+              </div>
+            </div>
+          </div>
+          <div class="pages fl-r mr0 mt20 mb20"><em class="fl-l lh23">共有0条，每页显示：20条</em>
+              <a href="" class="first">首页</a>
+              <a href="" class="prev">上一页</a><b class="cur">1</b>
+              <a href="">2</a>
+              <a href="">3</a>
+              <span class="ml10 mr10 fl-l in-block">...</span>
+              <a href="">4</a>
+              <a href="">5</a>
+              <a href="">6</a>
+              <a href="" class="next">下一页</a>
+              <a href="" class="last">尾页</a><span class="ml5 mr5 fl-l in-block"><input type="text"></span>
+              <a href="" class="go">GO</a>
+          </div>
+        </div>
+    </div>
+    <div class="right-filter">
+      <div class="title lh24 col-default">
+        筛选
+      </div>
+      <div class="search">
+        <input type="text" name="" value="" placeholder="法人或股东">
+        <a href="#" class="btn">
+          <i class="ico-search"></i>
+        </a>
+      </div>
+      <div class="select-panel after-cls">
+        <p class="f12 col-gray9">企业类型</p>
+        <select class="" name="">
+          <option value="">全部</option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+      <div class="select-panel after-cls">
+        <p class="f12 col-gray9"><em class="w84 mr10 in-block fl-l">注册资本</em><em class="w84 in-block fl-l">成立日期</em></p>
+        <select  name="" class="w94 mr10" id="province" onchange="selectProvince(this)">
+          <option value="">0</option>
+        </select>
+        <select  name="" class="w94" id="city">
+          <option value="">1990</option>
+        </select>
+      </div>
+      <div class="select-panel after-cls">
+        <p class="f12 col-gray9">行业门类</p>
+        <select class="" name="">
+          <option value="">全部</option>
+          <option value=""></option>
+          <option value=""></option>
+        </select>
+      </div>
+      <div class="select-panel after-cls">
+        <p class="f12 col-gray9">注册地</p>
+        <select class="" name="">
+          <option value="">浙江</option>
+          <option value="">浙江</option>
+          <option value="">浙江</option>
+        </select>
+      </div>
+      <div class="bottom after-cls">
+        <a href="javascript:;" class="a fl-l">
+          重新筛选
+        </a>
+        <a href="javascript:;" class="b fl-r">
+          高级筛选
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include_once('templete/pub_foot_a.php') ?>
+<script type="text/javascript">
+$(function(){
+})
+</script>
+</body>
+</html>
--- application/views/templete/pub_nav_a.php
+++ application/views/templete/pub_nav_a.php
@@ -3,10 +3,7 @@
     <div class="container after-cls">
         <a href="/page/home_london.php" class="main-logo fl-l mb10">公司合创网<em>官网<br/>旗下平台</em></a>
         <div class="nav-login pos-rela">
-            <a href="javascript:;" onclick="Pop.open('upwin_register')" class="zc">注册</a><em class="line ml10 mr10"></em><a href="javascript:;" onclick="Pop.open('upwin_login')" class="dl">登录</a>
-            <a href="/page/user-mine" class="use pos-rela" id="user">
-              用户中心
-            </a>
+            <a href="javascript:;" onclick="Pop.open('upwin_register')" class="zc">新用户注册</a><a href="javascript:;" onclick="Pop.open('upwin_login')" class="use">登录用户中心</a>
         </div>
         <div class="nav-box fl-r">
           <ul class="mr20">
@@ -14,10 +11,10 @@
               <a href="/page/home_london.php">首页</a>
             </li>
             <li>
-              <a href="/page/trading-center">公司股权交易</a>
+              <a href="/page/trading-center2.php">公司股权交易</a>
             </li>
             <li>
-              <a href="/page/company-index.php">公司信用查询</a>
+              <a href="/page/company-credit-list2.php">公司信用查询</a>
             </li>
             <li style="margin-right: 100px;">
               <a href="/page/news.php">公司官网</a>
