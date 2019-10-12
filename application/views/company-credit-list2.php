192c776e738c27c6ed1cec1bad552a73def9cfa8
footerbird <footerbird@163.com>
2019-10-12 17:02:26 +0800

    公司信用查询列表页面内容调整

--- application/views/company-credit-list2.php
+++ application/views/company-credit-list2.php
@@ -20,30 +20,30 @@
           <div class="box certified">
             <div class="top">
               <div class="title">
-                <a class="f18" href="/page/company-credit-page-simple.php">我是<em class="col-f4364c">简版</em>详情页 点我</a>
+                <a class="f18" href="/page/company-credit-page-simple.php"><em class="col-f4364c">完美</em>世界股份有限公司</a>
                 <a href="javascript:;" class="abtn-a-renzheng">认证</a>
                 <a href="javascript:;" class="abtn-a-zaiye">在业</a>
                 <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
                 <a href="#" class="abtn-a-guanzhu">关注</a>
               </div>
               <div class="info">
-                <span>法定代表人： 谢青弟</span>
-                <span>注册资本：1000万元人民币</span>
-                <span>成立时间：2003-08-01</span>
-                <span>所属行业：批发和零售业</span>
-                <span>地址：南京市鼓楼区建宁路37号</span>
+                <span>法定代表人： 池宇峰</span>
+                <span>注册资本：131479.9692万元人民币</span>
+                <span>成立时间：	1999-08-27</span>
+                <span>所属行业：	文化、体育和娱乐业</span>
+                <span>地址：德清县钟管镇龙山路117号</span>
               </div>
             </div>
             <div class="center">
               <ul class="list">
                 <li>
-                  <a href="#">名商网代理发布华为 P30 pro 购机享分期免息<em>2019-12-09</em></a>
+                  <a href="#">引领价值创造 完美世界曾映雪获CFO年度杰出人物<em>2019-10-09</em></a>
                 </li>
                 <li>
-                  <a href="#">新开发者限时优惠。低至9元起！<em>2019-12-09</em></a>
+                  <a href="#">“新财富金牌董秘”评选揭晓 完美世界王巍巍摘得榜眼<em>2019-11-09</em></a>
                 </li>
                 <li>
-                  <a href="#">名商网代理。华为交换机/服务器/存储等在线购买<em>2019-12-09</em></a>
+                  <a href="#">完美世界:手游业务增长抢眼 下半年精品游戏值得期待<em>2019-12-09</em></a>
                 </li>
               </ul>
               <table width="100%">
@@ -75,52 +75,28 @@
           <div class="box certified">
             <div class="top">
               <div class="title">
-                <a class="f18" href="/page/company-credit-page.php">我是<em class="col-f4364c">正常版</em>详情页 点我</a>
+                <a class="f18" href="/page/company-credit-page.php"><em class="col-f4364c">完美</em>数联（北京）科技有限公司</a>
                 <a href="javascript:;" class="abtn-a-renzheng">认证</a>
-                <a href="javascript:;" class="abtn-a-zaiye-had">在业</a>
-                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
+                <a href="javascript:;" class="abtn-a-zaiye">在业</a>
                 <a href="#" class="abtn-a-guanzhu had">已关注</a>
               </div>
               <div class="info">
-                <span>法定代表人： 谢青弟</span>
-                <span>注册资本：1000万元人民币</span>
+                <span>法定代表人： 杨卫国</span>
+                <span>注册资本：		2514万元人民币</span>
                 <span>成立时间：2003-08-01</span>
                 <span>所属行业：批发和零售业</span>
-                <span>地址：南京市鼓楼区建宁路37号</span>
+                <span>地址：北京市朝阳区望京园601号楼4层515</span>
               </div>
             </div>
             <div class="center">
               <ul class="list">
                 <li>
-                  <a href="#">名商网代理发布华为 P30 pro 购机享分期免息<em>2019-12-09</em></a>
+                  <a href="#">蚂蚁金服子公司2亿增资完美数联,欲打造00后“完美校园”<em>2019-10-01</em></a>
                 </li>
                 <li>
-                  <a href="#">新开发者限时优惠。低至9元起！<em>2019-12-09</em></a>
-                </li>
-                <li>
-                  <a href="#">名商网代理。华为交换机/服务器/存储等在线购买<em>2019-12-09</em></a>
+                  <a href="#">如何评价完美数联完成 2.5 亿人民币战略投资?<em>2019-11-05</em></a>
                 </li>
               </ul>
-              <table width="100%">
-                <thead>
-                  <tr>
-                    <th width="154">公司代码</th>
-                    <th width="154">出售股权</th>
-                    <th width="205">总交易额<em>（元/人民币）</em></th>
-                    <th width="205">最小交易额<em>（元/人民币）</em></th>
-                    <th width="205">历史成交额<em>（万元/人民币）</em></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>907701</td>
-                    <td>12%</td>
-                    <td>92000000</td>
-                    <td>50000</td>
-                    <td>12.5</td>
-                  </tr>
-                </tbody>
-              </table>
             </div>
             <div class="ft after-cls">
               <a href="#" class="buy3">暂无在售股权</a>
@@ -130,36 +106,34 @@
           <div class="box">
             <div class="top">
               <div class="title">
-                <a class="f18" href="/page/company-credit-page.php">我是<em class="col-f4364c">正常版</em>详情页 点我</a>
+                <a class="f18" href="/page/company-credit-page.php"><em class="col-f4364c">完美</em>幻境（北京）科技有限公司</a>
                 <a href="javascript:;" class="abtn-a-zaiye">在业</a>
-                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
                 <a href="#" class="abtn-a-guanzhu had">已关注</a>
                 <a href="#" class="abtn-a-kaitong">我是法人，申请开通官网</a>
               </div>
               <div class="info">
-                <span>法定代表人： 谢青弟</span>
-                <span>注册资本：1000万元人民币</span>
-                <span>成立时间：2003-08-01</span>
-                <span>所属行业：批发和零售业</span>
-                <span>地址：南京市鼓楼区建宁路37号</span>
+                <span>法定代表人： 赵博</span>
+                <span>注册资本：	153.0667万元人民币</span>
+                <span>成立时间：	2014-11-02</span>
+                <span>所属行业：科学研究和技术服务业</span>
+                <span>地址：北京市朝阳区东四环中路78号楼4层5B22</span>
               </div>
             </div>
           </div>
           <div class="box">
             <div class="top">
               <div class="title">
-                <a class="f18" href="/page/company-credit-page.php">我是<em class="col-f4364c">正常版</em>详情页 点我</a>
+                <a class="f18" href="/page/company-credit-page.php">北京<em class="col-f4364c">完美</em>视通科技有限公司</a>
                 <a href="javascript:;" class="abtn-a-zaiye-had">注销</a>
-                <span class="block-f3 col-f4364c">股权交易&nbsp;7000股</span>
                 <a href="#" class="abtn-a-guanzhu">关注</a>
                 <a href="#" class="abtn-a-kaitong">我是法人，申请开通官网</a>
               </div>
               <div class="info">
-                <span>法定代表人： 谢青弟</span>
-                <span>注册资本：1000万元人民币</span>
+                <span>法定代表人： 徐兴建</span>
+                <span>注册资本：500万元人民币</span>
                 <span>成立时间：2003-08-01</span>
-                <span>所属行业：批发和零售业</span>
-                <span>地址：南京市鼓楼区建宁路37号</span>
+                <span>所属行业：科学研究和技术服务业</span>
+                <span>地址：北京市海淀区上地七街1号2号楼四层406室 </span>
               </div>
             </div>
           </div>
