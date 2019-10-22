a6b99d3bde2b4ce47c48546d79e898c49032e212
xunijy <412294794@qq.com>
2019-10-22 10:39:18 +0800

    h5账户资料-联系方式

---
+++ application/views/mobile/user-account-connect.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+    <?php include_once('templete/pub_head.php') ?>
+    </head>
+    <body>
+    <header class="header">
+      <div class="header-title">
+        <a href="javascript:history.go(-1);" id="" class="back">
+          <i class="ico-header-back"></i>
+        </a>
+        <h4>联系方式</h4>
+      </div>
+    </header>
+    <div class="container pt44 pb50">
+      <div class="cont-box">
+        <div class="mine-set">
+          <div class="weui-cells form">
+            <div class="weui-cell">
+              <div class="weui-cell__bd">
+                <p>手机</p>
+              </div>
+              <div class="weui-cell__ft">
+                <input type="text" name="" value="182****3794" placeholder="请输入手机号码">
+              </div>
+            </div>
+            <div class="weui-cell">
+              <div class="weui-cell__bd">
+                <p>QQ</p>
+              </div>
+              <div class="weui-cell__ft">
+                <input type="text" name="" value="" placeholder="请输入QQ号">
+              </div>
+            </div>
+            <div class="weui-cell">
+              <div class="weui-cell__bd">
+                <p>邮箱</p>
+              </div>
+              <div class="weui-cell__ft">
+                <input type="text" name="" value="" placeholder="请输入电子邮箱">
+              </div>
+            </div>
+            <div class="weui-cell">
+              <div class="weui-cell__bd">
+                <p>微信</p>
+              </div>
+              <div class="weui-cell__ft">
+                <input type="text" name="" value="" placeholder="请输入微信号">
+              </div>
+            </div>
+            <div class="weui-cell">
+              <div class="weui-cell__bd">
+                <p>微博</p>
+              </div>
+              <div class="weui-cell__ft">
+                <input type="text" name="" value="" placeholder="请输入微博">
+              </div>
+            </div>
+            <div class="weui-cell">
+              <div class="weui-cell__bd">
+                <p>地址</p>
+              </div>
+              <div class="weui-cell__ft">
+                <input type="text" name="" value="" placeholder="请输入地址">
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="ta-c mt50 mb10">
+          <a class="btn-default" href="javascript:history.go(-1);">保存</a>
+        </div>
+      </div>
+    </div>
+    <?php include_once('templete/pub_foot.php') ?>
+    <script type="text/javascript">
+    $(function () {
+    })
+    </script>
+    </body>
+</html>
---
+++ application/views/mobile/user-account.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+    <?php include_once('templete/pub_head.php') ?>
+    </head>
+    <body>
+    <header class="header">
+      <div class="header-title">
+        <a href="javascript:history.go(-1);" id="" class="back">
+          <i class="ico-header-back"></i>
+        </a>
+        <h4>账户资料</h4>
+      </div>
+    </header>
+    <div class="container pt44 pb50">
+      <div class="cont-box">
+        <div class="mine-set">
+          <a href="/mobile/user-account-connect.php">设置联系方式</a>
+        </div>
+        <div class="ta-c mt50 mb10">
+          <a class="btn-default" href="javascript:history.go(-1);">保存</a>
+        </div>
+      </div>
+    </div>
+    <?php include_once('templete/pub_foot.php') ?>
+    <script type="text/javascript">
+    $(function () {
+    })
+    </script>
+    </body>
+</html>
