dcdc46f6f97cab51a9092d68171bea56091e4557
xunijy <412294794@qq.com>
2019-10-29 11:47:31 +0800

    h5关注的公司

---
+++ application/views/mobile/user-focus.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+    <?php include_once('templete/pub_head.php') ?>
+    </head>
+    <body>
+    <header class="header">
+      <div class="header-title">
+        <a href="javascript:history.go(-1);" id="" class="back">
+          <i class="ico-header-back"></i>
+        </a>
+        <h4>关注的公司</h4>
+      </div>
+    </header>
+    <div class="container pt44">
+      <div class="user-account">
+        <div class="bd mt15">
+          <div class="bd-items">
+            <a class="title" href="/mobile/company-credit-pages.php">
+              <span class="col-base">杭州网络有限公司</span>
+              <i class="ico-arrow-r"></i>
+            </a>
+            <div class="company-box">
+              <div class="hd">
+                <a href="javascript:;" class="cancel-btn">取消关注</a>
+              </div>
+              <div class="bd">
+              <dl class="info">
+                <dt>法定代表人：</dt>
+                <dd>浙江省</dd>
+              </dl>
+              <dl class="info">
+                <dt>经营状态：</dt>
+                <dd>在业</dd>
+              </dl>
+              <dl class="info">
+                <dt>官网认证：</dt>
+                <dd>已认证，股权交易中</dd>
+              </dl>
+              <dl class="info">
+                <dt>备注：</dt>
+                <dd class="ellip">计算机软件、通讯设备、自动化控制设备合伙人 计算机</dd>
+              </dl>
+              </div>
+            </div>
+          </div>
+          <div class="bd-items">
+            <a class="title" href="/mobile/company-credit-pages.php">
+              <span class="col-base">杭州网络有限公司</span>
+              <i class="ico-arrow-r"></i>
+            </a>
+            <div class="company-box">
+              <div class="hd">
+                <a href="javascript:;" class="cancel-btn">取消关注</a>
+              </div>
+              <div class="bd">
+              <dl class="info">
+                <dt>法定代表人：</dt>
+                <dd>浙江省</dd>
+              </dl>
+              <dl class="info">
+                <dt>经营状态：</dt>
+                <dd>在业</dd>
+              </dl>
+              <dl class="info">
+                <dt>官网认证：</dt>
+                <dd>已认证，股权交易中</dd>
+              </dl>
+              <dl class="info">
+                <dt>备注：</dt>
+                <dd class="ellip">计算机软件、通讯设备、自动化控制设备合伙人 计算机</dd>
+              </dl>
+              </div>
+            </div>
+          </div>
+          <div class="bd-items">
+            <div class="cocn-kaitong">
+              <p class="col-base f16 ta-c lh40">您尚未关注任何公司</p>
+              <p class="col-gray6 f12">您可以点击"<a href="/mobile/company-credit-list.php" class="col-3daaef">公司信用查询列表</a>"查询感兴趣的公司，快速查找投资对象
+              </p>
+              <div class="ta-c pt20 pb30">
+                <a href="/mobile/company-credit-list.php" class="btn-default">查看企业信用</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include_once('templete/pub_foot.php') ?>
+    <script type="text/javascript">
+    $(function () {
+    })
+    </script>
+    </body>
+</html>
