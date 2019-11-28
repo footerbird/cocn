845b3cb822ac85a79ac0e8202cf638322234a85d
xunijy <412294794@qq.com>
2019-11-28 17:11:23 +0800

    pc 专题页 为什么选择官网

---
+++ application/views/why_cocn.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php include_once('templete/pub_head_london.php') ?>
+</head>
+<body>
+<?php include_once('templete/pub_nav_a.php') ?>
+<!-- 轮播开始 -->
+<div class="after-cls pos-rela why_cocn-banner">
+  <div class="banner">
+    <div class="cont">
+      <div class="title"><img src="/htdocs/images/why-cocn-title.png" alt=""></div>
+      <div class="desc">
+        今天，哪些国家的公司在使用.cn+国家的域名？
+        <br />
+        英国、德国、意大利、日本、韩国、印度、南非、荷兰等23个国家...
+        <br />
+        <br />
+        .co.ag .co.at .co.cr .co.cz .co.de .co.ee .co.hu .co.il .co.in .co.it  ...
+      </div>
+      <div class="btn">
+        <a href="#">公司法人，选择.co.cn域名&nbsp;&nbsp;&gt;</a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="bg-white pb25 pt20">
+  <div class="container pl15 after-cls">
+    <div class="fl-l">
+      <div class="f30 mt50 mb20">不懂技术，也能驾驭功能强大的官网</div>
+      <p class="f16 lh30 pb10 col-f4364c">新平台上线，限时免费送！手慢无</p>
+      <p class="ta-r mt50">
+        <a href="#" class="fl-r col-3daaef f16">了解更多&gt;</a>
+      </p>
+    </div>
+    <div class="fl-r mt20 mb40">
+      <img src="/htdocs/images/why-cocn-1.png" alt="" class="in-block fl-r">
+    </div>
+  </div>
+</div>
+<div class="bg-f2f6f8 pt40 pb40">
+  <div class="container">
+    <ul class="why-cocn-cont after-cls">
+      <li class="a">
+        <div class="tit">
+          公司股权交易
+        </div>
+        <div class="desc">
+          股权交易可以让公司咋短期内获得较好
+          <br />
+          的资金支持，让公司有更大的发展。
+        </div>
+      </li>
+      <li class="b">
+        <div class="tit">
+          公司信用查询
+        </div>
+        <div class="desc">
+          信用查询是公司经营的基础，良好的信用可以
+          <br />
+          让公司经营的更好
+        </div>
+      </li>
+      <li class="c">
+        <div class="tit">
+          公司官网
+        </div>
+        <div class="desc">
+          通过官网发布官方信息
+          <br />
+          确保信息的准确和权威
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="bg-white pb20 pt50">
+  <div class="container pl15 after-cls">
+    <div class="fl-l pt50">
+      <div class="f30 mt50 mb20 pt50">为什么要选择官网</div>
+      <p class="f16 lh30 pb10">我们不仅有非常合适的域名，还有比较完善的服务</p>
+    </div>
+    <div class="fl-r mt20 mb40 mr50">
+      <img src="/htdocs/images/why-cocn-5.png" alt="" class="in-block fl-r">
+    </div>
+  </div>
+</div>
+<div class="bg-f2f6f8 pb50 pt50">
+  <div class="container pl15 after-cls">
+    <div class="fl-l">
+      <div class="f30 mb20">官网自助建站常见问题</div>
+      <p class="f16 lh30 pb10">
+        申请公司官网，需要什么样的条件？
+        <br/>
+        只有持有有效营业执照的法人（或法人授权）才能开通官网<br/><br/>
+        <b>选择域名要哪些要诀？<br/></b>
+        选择一个便于人们识记拼写的域名，此方法有助于打造您的品牌和信誉，我们建议使用公司字号全称，以及公司经营相关的其<br/>
+        他信息，比如：公司经营地、行业等。良好的辨识度会让公司潜在的客户和网站访问者更容易了解您。<br/><br/>
+        <b>如果我需要帮助选择正确的域名怎么办？<br/></b>
+        在您申请公司认证时，我们的品牌管家以及为您选择了我们认为合适的域名。如果在域名使用方面仍有疑问，您可以向我们的<br/>
+        支持团队寻求帮助，我们将随时等待您的咨询，帮助您完成域名注册。
+      </p>
+    </div>
+  </div>
+</div>
+<div class="bg-white pb25 pt25">
+  <div class="container ta-c f16">
+    <a href="#" class="col-3daaef f16 mb15 in-block">查看平台用户协议和隐私政策</a>
+    <p>第三方徽记和标记均为其各自所有者的注册商标。保留所有权利。</p>
+  </div>
+</div>
+<div class="bg-393939 pb40 pt40">
+  <div class="container ta-c f30 col-white">
+    <p>需要帮助吗？请致电我们屡获好评的支持团队：0571-88365365</p>
+  </div>
+</div>
+<!-- footer -->
+<?php include_once('templete/pub_foot_a.php') ?>
+<script type="text/javascript">
+</script>
+</body>
+</html>
