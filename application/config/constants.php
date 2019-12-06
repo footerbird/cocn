9461e69ee08cb4bb874d7c38bd68f6b06e740e77
xunijy <412294794@qq.com>
2019-12-06 16:54:44 +0800

    pc 优化路径

--- application/config/constants.php
+++ application/config/constants.php
@@ -12,7 +12,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //更新CSS JS缓存时间
-define('CACHE_TIME', '20191202001025');
+define('CACHE_TIME', '20191206001527');
 
 defined('SHOW_DEBUG_BACKTRACE') OR define('SHOW_DEBUG_BACKTRACE', TRUE);
 
